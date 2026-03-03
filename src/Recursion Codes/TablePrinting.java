bcfbfc878710897ab7788553fbd732b2c6db3e1a
Aditya Suryavanshi <160452055+adityasuryavanshi2311@users.noreply.github.com>
2026-03-03 19:29:44 +0530

    Fix base case in recursive table printing

--- src/Recursion Codes/TablePrinting.java
+++ src/Recursion Codes/TablePrinting.java
@@ -4,10 +4,11 @@ public class TablePrinting {
 
   public static void printTable(int number, int i) {
 
-    if(i <= 10) {
-      System.out.printf("\n%d X %d = %d", number,i,number*i);
-      printTable(number, i+1);
+    if(i == 11) {
+      return;
     }
+    System.out.printf("\n%d X %d = %d", number,i,number*i);
+    printTable(number, i+1);
   } 
 
   public static void main(String args[]) {
