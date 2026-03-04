0d218385e90f4095f0bc429974b09cabf4273445
Aditya Suryavanshi <160452055+adityasuryavanshi2311@users.noreply.github.com>
2026-03-04 20:39:58 +0530

    Add recursion for printing numbers in ascending and descending order

--- src/Recursion Codes/numberPrinting.java
+++ src/Recursion Codes/numberPrinting.java
@@ -12,6 +12,16 @@ public class numberPrinting {
 		System.out.println(number); // prints number
 		recurFunction(number + 1, endNum); //calls it self by passing incremented number and ending number
 	}
+
+	public static void recurFunctionReverse(int number, int endNum) {
+		
+		if(number == endNum+1){
+			//checks base condition
+			return; // returns to main when condition beacomes true
+		}
+		System.out.println(number); // prints number
+		recurFunction(number - 1, endNum); //calls it self by passing incremented number and ending number
+	}
 	
 	public static void main(String args[]) {
 		
@@ -19,12 +29,19 @@ public class numberPrinting {
 		
 		System.out.print("Enter a number: "); // user input
 		int endNum = input.nextInt(); // ending number to stop function
+
+		System.out.println("Enter 1 for ascending order or 2 for descending order: ");
+		int userChoice = input.nextInt();
 		
 		int number = 1; // starting index
 		
-		recurFunction(number, endNum); // calls function by passing number(1) and ending index by input
+		if(userChoice == 1) {
+			recurFunction(number, endNum);
+		}else if(userChoice == 2) {
+			recurFunctionReverse(number, endNum);
+		}
+		else {
+			System.out.println("OOPs, Wrong input!");
+		}
 	}
 }
-
-
-
