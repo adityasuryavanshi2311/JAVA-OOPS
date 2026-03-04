071495a7845157f6b49bb83b1ec6dddcb1c98f6f
Aditya Suryavanshi <160452055+adityasuryavanshi2311@users.noreply.github.com>
2026-03-04 20:27:53 +0530

    Add files via upload

---
+++ src/Recursion Codes/numberPrinting.java
@@ -0,0 +1,30 @@
+//Recursion = where function calls it self called recurdion
+import java.util.Scanner;
+
+public class numberPrinting {
+	
+	public static void recurFunction(int number, int endNum) {
+		
+		if(number == endNum+1){
+			//checks base condition
+			return; // returns to main when condition beacomes true
+		}
+		System.out.println(number); // prints number
+		recurFunction(number + 1, endNum); //calls it self by passing incremented number and ending number
+	}
+	
+	public static void main(String args[]) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number: "); // user input
+		int endNum = input.nextInt(); // ending number to stop function
+		
+		int number = 1; // starting index
+		
+		recurFunction(number,); // calls function by passing number(1) and ending index by input
+	}
+}
+
+
+
