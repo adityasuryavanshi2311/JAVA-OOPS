402cf686d526fb2581a5c4343d936245a57313ed
Aditya Suryavanshi <160452055+adityasuryavanshi2311@users.noreply.github.com>
2026-03-06 08:45:03 +0530

    Refactor FibonacciSeries printSeries method and fixed parameters and arguments
    Refactored FibonacciSeries to simplify parameters and logic.

--- src/Recursion Codes/FibonacciSeries.java
+++ src/Recursion Codes/FibonacciSeries.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 public class FibonacciSeries {
 	
-	public static void printSeries(int number, int a, int b, int c) {
-		int i = 0;
-	  if(i == number) {
+	public static void printSeries(int number, int a, int b) {
+	  	if(number == 0) {
 			return;
 		}
+		int c = a + b;
 		System.out.println(c);
-		a = b;
-		b = c;
-		c = a = b;
-		printSeries(c);
+		printSeries(number-1, b, c);
  	}
 	
 	public static void main(String args[]) {
@@ -21,9 +18,10 @@ public class FibonacciSeries {
 		System.out.print("Enter a number for series: ");
 		int number = input.nextInt();
 
-		int a=0,b=1,c=0;
-		
-		printSeries(number, a, b, c);
+		int a=0,b=1;
+		System.out.println(a);
+		System.out.println(b);
+;		printSeries(number, a, b);
 	}
 }
 
